931780a754a50083afafc76a5332699a3c493351
Radivarig <reslav.hollos@gmail.com>
2016-01-27 13:55:16 +0100

    (add) init script for setting up cabal

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+# (todo) if sandbox then confirm reinit, else init
+# rm -rf .cabal-sandbox; rm .cabal.sandbox.config
+cabal sandbox init
+cabal update
+cabal install HaskellNet HaskellNet-SSL text
+
