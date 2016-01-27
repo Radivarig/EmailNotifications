21d920877c3987426ad1b7a74c178d1ae275cf89
Radivarig <reslav.hollos@gmail.com>
2016-01-27 02:26:57 +0100

    (add) EmailConfig module

---
+++ src/EmailConfig.hs
@@ -0,0 +1,19 @@
+module EmailConfig where
+
+data Configuration = Configuration {
+  host        :: String,
+  port        :: Maybe Int,
+  username    :: String,
+  password    :: String
+} deriving (Show, Read)
+
+-- conf :: Configuration
+-- conf = Configuration "smtp.gmail.com" Nothing "user.name@gmail.com" "password"
+-- saveConfig :: IO ()
+-- saveConfig = writeFile "config.txt" $ show conf
+
+readConfig :: IO Configuration
+readConfig = do
+  c <- readFile "config.txt"
+  let config = read c :: Configuration
+  return config
